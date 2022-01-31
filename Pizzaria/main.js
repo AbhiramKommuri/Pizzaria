4bbd44d28d4eae1655709dc6ade20a12d2cef06e
AbhiramKommuri <73000161+AbhiramKommuri@users.noreply.github.com>
2022-01-30 19:48:08 -0600

    Add files via upload

---
+++ Pizzaria/main.js
@@ -0,0 +1,32 @@
+menu_list_array = ["Veg Supreme Pizza",
+                   "Paneer Tikka Pizza",
+                   "Deluxe Veggie Pizza",
+                   "Veg Extravagansa Pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata = "<ol class='menulist'";
+menu_list_array.sort();
+
+
+for(var i=0; i< menu_list_array.length(); i++){
+    htmldata = htmldata + "<li>" + menu_list_array[i] + "</li>"
+    };
+
+htmldata = htmldata + "</ol>"
+document.getElementById("display_menu")
+};
+
+function add_item(){
+var htmldata;
+item-document.getElementById("add_Item").value;
+menu_list_array.push(item);
+menu_list_array.sort();
+htmldata = "<section class= 'cards'>"
+
+for(var i=0; i< menu_list_array.length(); i++){
+htmldata = htmldata + "<div class= 'cards'>" + "<img src='pizzaImg.png'>" + menu_list_array[i]; + "</div>"
+}
+htmldata = htmldata + "</section>"
+document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
